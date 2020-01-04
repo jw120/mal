b65035b5b223d5b2e1590e6d88078f3ed4160239
Joe Watson <joe_watson@mail.com>
2020-01-04 17:03:16 +0900

    c step 0 working

--- jw-c/src/step0_repl.c
+++ jw-c/src/step0_repl.c
@@ -1,6 +1,51 @@
 // Step 0 - repl
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT_BUFFER_SIZE 200
+
+const char *READ(const char * s)
+{
+    return s;
+}
+
+const char *EVAL(const char * s)
+{
+    return s;
+}
+
+const char *PRINT(const char* s)
+{
+    return s;
+}
+
+const char *rep(const char *s)
+{
+    return PRINT(EVAL(READ(s)));
+}
+
 int main()
 {
+    while (true) {
 
+        char input_buffer[INPUT_BUFFER_SIZE];
+
+        printf("user> ");
+        fflush(stdout);
+
+        if (fgets(input_buffer, INPUT_BUFFER_SIZE, stdin) == NULL) {
+            exit(!feof(stdin));
+        }
+
+        // remove trailing newline
+        if (strlen(input_buffer) > 0) {
+            input_buffer[strlen(input_buffer) - 1] = '\0';
+        }
+
+        puts(rep(input_buffer));
+    }
+    return 0;
 }
