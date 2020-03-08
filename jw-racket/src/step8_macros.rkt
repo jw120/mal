f9bc72bb2f361b653c8233415158c8800b4718c7
Joe Watson <joe_watson@mail.com>
2020-03-08 15:30:09 +0800

    Started step 8

---
+++ jw-racket/src/step8_macros.rkt
@@ -0,0 +1,135 @@
+#lang racket/base
+
+(require racket/class
+         racket/list
+         racket/match
+         racket/vector
+         "core.rkt"
+         "env.rkt"
+         "exceptions.rkt"
+         "printer.rkt"
+         "reader.rkt"
+         "repl.rkt"
+         "utils.rkt")
+
+(define (READ s) (read_string s))
+
+(define (EVAL ast env)
+  (cond
+    [(not (list? ast)) (eval_ast ast env)]
+    [(null? ast) ast]
+    [else
+     (let ([head (car ast)]
+           [args (cdr ast)])
+       (match head
+         ['def! (def-special-form args env)]
+         ['do (do-special-form args env)]
+         ['fn* (fn-special-form args env)]
+         ['if (if-special-form args env)]
+         ['let* (let-special-form args env)]
+         ['quasiquote (EVAL (mal-quasi-quote (car args)) env)]
+         ['quote (quote-special-form args env)]
+         [else
+          (let* ([evaluated-ast (eval_ast ast env)]
+                 [evaluated-head (car evaluated-ast)]
+                 [evaluated-args (cdr evaluated-ast)])
+            (cond
+              [(procedure? evaluated-head) (apply evaluated-head evaluated-args)]
+              [else (raise-mal-eval "Cannot apply non-procedure")]))]))]))
+
+(define (def-special-form args env)
+  (unless (and (equal? 2 (length args)) (symbol? (car args)))
+    (raise-mal-eval "Bad arguments to def!"))
+  (let ([sym (car args)]
+        [val (EVAL (cadr args) env)])
+    (send env set sym val)
+    val))
+
+(define (do-special-form args env)
+  (when (empty? args)
+    (raise-mal-eval "No arguments to do"))
+  (for/last ([a args])
+    (EVAL a env)))
+
+(define (fn-special-form args env)
+  (match args
+    [(list fn-binds fn-val)
+     (lambda closure-args
+       (let ([closure-env (new env% [outer env] [binds (list-or-vector->list fn-binds)] [exprs closure-args])])
+         (EVAL fn-val closure-env)))]
+    [_ (raise-mal-eval "Bad arguments to fn*")]))
+
+(define (if-special-form args env)
+  (unless (or (equal? (length args) 2) (equal? (length args) 3))
+    (raise-mal-eval "Bad arguments to if"))
+  (define test-value (EVAL (first args) env))
+  (if (or (equal? test-value nil) (equal? test-value #f))
+      (if (equal? (length args) 2) nil (EVAL (third args) env))
+      (EVAL (second args) env)))
+
+(define (let-special-form args env)
+  (unless (and (equal? 2 (length args)) (list-or-vector? (car args)))
+    (raise-mal-eval "Bad arguments to let*"))
+  (let ([binding-list (list-or-vector->list (car args))]
+        [val (cadr args)]
+        [let-env (new env% [outer env])])
+    (send let-env bind-alternating-list binding-list EVAL)
+    (EVAL val let-env)))
+
+(define (quote-special-form args env)
+  (when (empty? args)
+    (raise-mal-eval "No arguments to quote"))
+  (car args))
+
+(define (eval_ast ast env)
+  (cond
+    [(nil? ast) ast]
+    ((boolean? ast) ast)
+    [(symbol? ast) (send env get ast)]
+    [(list? ast) (map (λ (x) (EVAL x env)) ast)]
+    [(vector? ast) (vector-map (λ (x) (EVAL x env)) ast)]
+    [(hash? ast) (make-immutable-hash (map (λ (k) (cons k (EVAL (hash-ref ast k) env))) (hash-keys ast)))]
+    [else ast]))
+
+(define (mal-quasi-quote raw-ast)
+  (define ast (if (vector? raw-ast) (vector->list raw-ast) raw-ast))
+  (cond
+    [(not (pair? ast)) (list 'quote ast)]
+    [else (let ([head (car ast)]
+                [rest (cdr ast)])
+            (cond
+              [(equal? head 'unquote)
+               (car rest)]
+              [(and (pair? head) (equal? (car head) 'splice-unquote))
+               (list 'concat (cadr head) (mal-quasi-quote rest))]
+              [else
+               (list 'cons (mal-quasi-quote head) (mal-quasi-quote rest))]))]))
+
+(define (PRINT s) (pr_str s #t))
+
+(define repl_env (new env%))
+(define (rep s) (PRINT (EVAL (READ s) repl_env)))
+
+; Add bindings from ns (from core)
+(for ([binding-pair ns])
+  (send repl_env set (car binding-pair) (cdr binding-pair)))
+
+; Add additonal bindings
+(send repl_env set 'eval (λ (ast) (EVAL ast repl_env)))
+(send repl_env set '*ARGV* '())
+
+; Add mal-defined functions
+(define mal-defs
+  '("(def! not (fn* (a) (if a false true)))"
+    "(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \"\\nnil)\")))))"))
+(for ([d mal-defs])
+  (rep d))
+
+; Either start the repl or load the given file
+(cond
+  [(vector-empty? (current-command-line-arguments))
+   (repl rep)]
+  [else
+   (send repl_env set '*ARGV* (vector->list (vector-drop (current-command-line-arguments) 1)))
+    (rep (format "(load-file ~s)" (vector-ref (current-command-line-arguments) 0)))
+    (void)]) ; void to avoid returning the value from rep
