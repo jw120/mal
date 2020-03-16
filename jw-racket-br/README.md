f41ec1c35b71417bf54875d1e9ee90745b6d18a4
Joe Watson <joe_watson@mail.com>
2020-03-16 08:45:41 +0800

    br starting to work

--- jw-racket-br/README.md
+++ jw-racket-br/README.md
@@ -1,8 +1,17 @@
 # Racket implementation of mal a la Beautiful Racket
 
+Implementation of mal as a translation to racket
+- repl
+- #lang
+- Dr Racket repl (a la Basic in BR)
+
+Next steps
+ - Get step2 to work
+   - Print outputs in mal form
+   - Handle '()
+
 
 Todo
-- Get tests to work
 - Stop using #lang br
 
 raco pkg install --auto beautiful-racket
--- jw-racket-br/brmal/repl.rkt
+++ jw-racket-br/brmal/repl.rkt
@@ -1,30 +1,22 @@
 #lang br
 
-(require readline/readline brmal/parser brmal/tokenizer brmal/core)
+(require brmal/core
+         brmal/parser
+         brmal/tokenizer
+         readline/readline)
 
-(displayln "repl-start")
-;(define (repl)
-(define s (readline "user> "))
-(define the-parse-tree (parse-to-datum (make-tokenizer (open-input-string s))))
-(display "repl-parsed: ")
-(displayln the-parse-tree)
-;(define expanded-form (strip-bindings
-;                      #`(module brmal brmal/expander
-;                          #,the-parse-tree)))
-(define stripped-form (strip-bindings the-parse-tree))
-(display "repl-stripped: ")
-(displayln stripped-form)
-(define ns (module->namespace 'brmal/core))
-(define val (eval stripped-form ns))
-(display "repl-evaled: ")
-(displayln val)
-;(repl)
-;  (if (eq? s eof)
-;      (displayln "") ; On Ctrl-D, clean up by printing a newline
-;      (begin
-;        (define the-parse-tree (parse (make-tokenizer (open-input-string s))))
-;        (define expanded-form (strip-bindings
-;                      #`(module brmal brmal/expander
-;                          #,the-parse-tree)))
-;        (print (eval expanded-form))
-;        (repl))))
+(define mal-namespace (module->namespace 'brmal/core))
+
+(define (repl)
+  (define input-string (readline "user> "))
+  (cond
+    [(eq? input-string eof)
+     (displayln "")] ; On Ctrl-D, clean up by printing a newline
+    [else
+     (define input-parsed (parse-to-datum (make-tokenizer (open-input-string input-string))))
+     (with-handlers
+       ([exn:fail? (λ (exn) (displayln (exn-message exn)))])
+       (define input-evaluated (eval input-parsed mal-namespace))
+       (displayln input-evaluated))
+     (repl)]))
+(repl)
