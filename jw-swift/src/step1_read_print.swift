3c537a3b35e56a30282be74fc711c91a719377c2
Joe Watson <joe_watson@mail.com>
2020-06-11 16:19:12 +0800

    Started swift 1 again

---
+++ jw-swift/src/step1_read_print.swift
@@ -0,0 +1,30 @@
+// jw-swift implementation of mal
+//
+// (C) Joe Watson 2020-06-11
+//
+// step 1 - read and print asts
+
+func READ(_ input: String) -> String {
+    return input
+}
+
+func EVAL(_ ast: String) -> String {
+    return ast
+}
+
+func PRINT(_ ast: String) -> String {
+    return ast
+}
+
+func rep(_ s: String) -> String {
+    return PRINT(EVAL(READ(s)))
+}
+
+while true {
+    print("user> ", terminator: "")
+    if let s = readLine() {
+        print(rep(s))
+    } else {
+        break
+    }
+}
