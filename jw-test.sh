dedc102e8279ace148f249f45d6b223b0daad951
Joe Watson <joe_watson@mail.com>
2020-11-08 16:42:52 +0800

    Added hosted test

--- jw-test.sh
+++ jw-test.sh
@@ -8,8 +8,10 @@ do
   echo "$impl"
   if [ "$impl" = "jw-racket" ]
   then
-    echo "Expect 14 soft failures on step A (meta not implemented)"
+    echo "Expect 14 soft failures on step A as meta not implemented (same for hosted)"
   fi
   make "test^$impl" | grep "failing\|TEST RESULTS" | grep -v "   0:"
+  echo "mal hosted on $impl"
+  make MAL_IMPL="$impl" test^mal | grep "failing\|TEST RESULTS" | grep -v "   0:"
 done
 
