b9bbe524031084080025b0b2b9b749ae9212ba53
Joe Watson <joe_watson@mail.com>
2020-11-09 23:03:17 +0800

    Work on reader

--- impls/jw-typed-racket/src/reader.rkt
+++ impls/jw-typed-racket/src/reader.rkt
@@ -1,4 +1,6 @@
-#lang typed/racket/base
+#lang typed/racket
+
+(require "types.rkt")
 
 ;(require racket/class
 ;         racket/match
@@ -6,50 +8,46 @@
 ;         ;"exceptions.rkt"
 ;         "utils.rkt")
 
-(provide read_string)
+;(provide read_string)
+
+(define (raise-mal-fail e) (error e))
+(define (raise-mal-read e) (error e))
 
 ; We use the symbols eof (defined by system) and white-space (defined here)
 ; to match inside this file (end of input and spaces/comments respectively)
 ; They are converted to the empty exception in read_string
-(define white-space (string->uninterned-symbol "white-space"))
+;(define white-space (string->uninterned-symbol "white-space"))
 
 ;; top-level reading function which we export. Sets up reader and hands over to read-form
 ;; If no input (or just space/comment raise the empty exception
-(define (read_string s)
-  (let ([f (read-form (new token-reader% [target-string s]))])
-    (when (or (eof-object? f) (equal? white-space f))
-      (raise-mal-empty))
-    f))
+(define (read_string [s : String]) : (U String EOF)
+  (read-form (reader 0 s)))
 
-;; class for stateful reader object
-(define token-reader%
-  (class object%
-    (init target-string)
-    (define current-pos 0)
-    (define current-string target-string)
-    (super-new)
-    (define/private (get-token advance-pos?)
-      (cond
-        [(>= current-pos (string-length current-string))
-         eof]
-        [else
-         (let ([match-list (regexp-match-positions mal-regexp current-string current-pos)])
-           (unless (equal? 2 (length match-list))
-             (raise-mal-fail "Unexpected in matching mal-regexp"))
-           (define group-start (caadr match-list))
-           (define group-end (cdadr match-list))
-           (when advance-pos? (set! current-pos group-end))
-           (if (equal? group-start group-end) ; Zero-length match (i.e., nothing except whitespace)
-               white-space
-               (substring current-string group-start group-end)))]))
-    (define/public (next-token)
-      (get-token #t))
-    (define/public (peek-token)
-      (get-token #f))))
+;; Reader is a struct which can be mutated when read
+(struct reader ([pos : Nonnegative-Integer] [str : String]) #:mutable)
+
+(define (get-token! [r : reader] [advance : Boolean]) : (U String EOF 'whitespace)
+  (let ([pos (reader-pos r)]
+        [str (reader-str r)])
+    (if (>= pos (string-length str))
+        eof
+        (match (regexp-match-positions mal-regexp str pos)
+          [(list (cons group-start group-end))
+           (when advance (set-reader-pos! r group-end))
+           (if (equal? group-start group-end) ; Zero-length match means nothing but white-space
+               'whitespace
+               (substring str group-start group-end))]
+          [_ (error "Unexpected in matching mal-regexp")]))))
+
+(define (next-token! [r : reader]) : (U String EOF 'whitespace)
+  (get-token! r #t))
+
+(define (peek-token [r : reader]) : (U String EOF 'whitespace)
+  (get-token! r #f))
 
 ;; top-level internal function to read from our reader
 (define (read-form r)
-  (match (send r peek-token)
+  (match (peek-token r)
     ["(" (read-list r)]
     ["[" (read-vector r)]
     ["{" (read-hash-map r)]
@@ -57,13 +55,13 @@
 
 ;; read a list
 (define (read-list r)
-  (unless (equal? (send r next-token) "(")
+  (unless (equal? (next-token! r) "(")
     (raise-mal-fail "no opening paren in read-list"))
   (read-forms-until ")" r))
 
 ;; read a vector
 (define (read-vector r)
-  (unless (equal? (send r next-token) "[")
+  (unless (equal? (next-token! r) "[")
     (raise-mal-fail "no opening bracker in read-vector"))
   (vector->immutable-vector
    (list->vector
@@ -81,7 +79,7 @@
     (cond
       [(equal? next-form end-token) '()]
       [(eof-object? next-form) (raise-mal-read "EOF found reading list or vector")]
-      [(equal? next-form white-space)  (read-forms-until end-token r)]
+      [(equal? next-form 'whitespace)  (read-forms-until end-token r)]
       [else (cons next-form (read-forms-until end-token r))])))
 
 ;; read an atom
@@ -90,7 +88,7 @@
   (match t
     ["true" #t]
     ["false" #f]
-    ["nil" nil]
+    ["nil" (mal-nil)]
     ["'" (list 'quote (read-form r))]
     ["`" (list 'quasiquote (read-form r))]
     ["~" (list 'unquote (read-form r))]
@@ -105,14 +103,31 @@
     [(regexp #rx"^\\\".*\\\"$") (remove-escapes (substring t 1 (- (string-length t) 1)))] ; quoted string
     [(regexp #rx"^\\\".*$") (raise-mal-read "EOF reading string")]
     [(regexp #rx"^:.+$")  (string->keyword (substring t 1))] ; keyword
-    [(regexp #rx"^;") white-space] ; comment
+    [(regexp #rx"^;") 'whitespace] ; comment
     [_ (cond
          [(eof-object? t) t]
-         [(equal? t white-space) t]
+         [(equal? t 'whitespace) t]
          [else (string->symbol t)])]))
 
+(define (remove-escapes s)
+  (let-values
+      ([(result still-in-escape)
+        (for/fold ([acc ""]
+                   [in-escape #f])
+                  ([ch (in-string s)])
+          (match (list in-escape ch)
+            [(list #t #\n) (values (string-append acc "\n") #f)]
+            [(list #t #\") (values (string-append acc "\"") #f)]
+            [(list #t #\\) (values (string-append acc "\\") #f)]
+            [(list #t x) (raise-mal-read (format "Unknown escape ~a in remove-escapes" ch))]
+            [(list #f #\\) (values acc #t)]
+            [(list #f x) (values (string-append acc (string ch)) #f)]))])
+    (when still-in-escape (raise-mal-read "Escape unbalanced in remove-escapes"))
+    result))
+
+
 ;; regexp for our tokens (at end of file to avoid confusing our editors highlighting)
-(define mal-regexp
+(define mal-regexp : Regexp
   (pregexp
    (string-append
     "[\\s,]*" ; leading white-space (including commas) skipped (as outside of regexp group)
@@ -129,57 +144,57 @@
 (module+ test
   (require rackunit)
 
-   ; empty input gives exceptions
-   (check-exn exn:mal:empty? (λ () (read_string "")) "Empty string")
-   (check-exn exn:mal:empty? (λ () (read_string "  ")) "White space")
-   (check-exn exn:mal:empty? (λ () (read_string ";qqq")) "Comment only")
+  ; empty input gives exceptions
+  (check-exn exn:mal:empty? (λ () (read_string "")) "Empty string")
+  (check-exn exn:mal:empty? (λ () (read_string "  ")) "White space")
+  (check-exn exn:mal:empty? (λ () (read_string ";qqq")) "Comment only")
 
-   ; sequences
-   (check-equal? (read_string "(1 2 3)") '(1 2 3) "List")
-   (check-equal? (read_string "()") '() "Empty list")
-   (check-equal? (read_string "[1 2 3]") #(1 2 3) "Vector")
-   (check-equal? (read_string "[1,2,4]") #(1 2 4) "Vector with commas")
-   (check-equal? (read_string "[]") #() "Empty vector")
-   (check-equal? (read_string "(1 (2 3))") '(1 (2 3)) "List of lists")
-   (check-equal? (read_string "(()())") '(()()) "Empty list of empty lists")
-   (check-equal? (read_string "{ \"a\" 1 \"bb\" 2}") #hash(("a" . 1) ("bb" . 2)) "Hash map")
-   (check-equal? (read_string "(()())") '(()()) "Empty hash map")
-   (check-exn exn:mal:read? (λ () (read_string "(1 2")) "Unterminated list")
-   (check-exn exn:mal:read? (λ () (read_string "(1 2]")) "Wrongly terminated list")
-   (check-exn exn:mal:read? (λ () (read_string "[1 2")) "Unterminated vector")
-   (check-exn exn:mal:read? (λ () (read_string "{1 2")) "Unterminated hash map")
+  ; sequences
+  (check-equal? (read_string "(1 2 3)") '(1 2 3) "List")
+  (check-equal? (read_string "()") '() "Empty list")
+  (check-equal? (read_string "[1 2 3]") #(1 2 3) "Vector")
+  (check-equal? (read_string "[1,2,4]") #(1 2 4) "Vector with commas")
+  (check-equal? (read_string "[]") #() "Empty vector")
+  (check-equal? (read_string "(1 (2 3))") '(1 (2 3)) "List of lists")
+  (check-equal? (read_string "(()())") '(()()) "Empty list of empty lists")
+  (check-equal? (read_string "{ \"a\" 1 \"bb\" 2}") #hash(("a" . 1) ("bb" . 2)) "Hash map")
+  (check-equal? (read_string "(()())") '(()()) "Empty hash map")
+  (check-exn exn:mal:read? (λ () (read_string "(1 2")) "Unterminated list")
+  (check-exn exn:mal:read? (λ () (read_string "(1 2]")) "Wrongly terminated list")
+  (check-exn exn:mal:read? (λ () (read_string "[1 2")) "Unterminated vector")
+  (check-exn exn:mal:read? (λ () (read_string "{1 2")) "Unterminated hash map")
 
-   ; List with comment inside
-   (check-equal? (read_string "(1 2 ;comment\n3)") '(1 2 3) "List with comment")
+  ; List with comment inside
+  (check-equal? (read_string "(1 2 ;comment\n3)") '(1 2 3) "List with comment")
 
-   ; strings
-   (check-equal? (read_string "\"pq\"") "pq" "String")
-   (check-equal? (read_string "\"\"") "" "Empty string")
-   (check-exn exn:mal:read? (λ () (read_string "\"abc")) "Unterminated string")
+  ; strings
+  (check-equal? (read_string "\"pq\"") "pq" "String")
+  (check-equal? (read_string "\"\"") "" "Empty string")
+  (check-exn exn:mal:read? (λ () (read_string "\"abc")) "Unterminated string")
 
-   ; numbers
-   (check-equal? (read_string "123") 123 "Number")
-   (check-equal? (read_string "-45") -45 "Negative number")
+  ; numbers
+  (check-equal? (read_string "123") 123 "Number")
+  (check-equal? (read_string "-45") -45 "Negative number")
 
-   ; special symbols
-   (check-equal? (read_string "'pqr") ''pqr "Quote")
-   (check-equal? (read_string "`pqr") '`pqr "Quasiquote")
-   (check-equal? (read_string "~pqr") ',pqr "Unquote")
-   (check-equal? (read_string "^pqr abc") '(with-meta abc pqr) "With-meta")
-   (check-equal? (read_string "@pqr") '(deref pqr) "Deref")
-   (check-equal? (read_string "~@pqr") '(splice-unquote pqr) "Splice-unquote")
+  ; special symbols
+  (check-equal? (read_string "'pqr") ''pqr "Quote")
+  (check-equal? (read_string "`pqr") '`pqr "Quasiquote")
+  (check-equal? (read_string "~pqr") ',pqr "Unquote")
+  (check-equal? (read_string "^pqr abc") '(with-meta abc pqr) "With-meta")
+  (check-equal? (read_string "@pqr") '(deref pqr) "Deref")
+  (check-equal? (read_string "~@pqr") '(splice-unquote pqr) "Splice-unquote")
 
-   ; keyword
-   (check-equal? (read_string ":pqr") '#:pqr "Keyword")
+  ; keyword
+  (check-equal? (read_string ":pqr") '#:pqr "Keyword")
 
-   ; symbol
-   (check-equal? (read_string "pqr") 'pqr "Symbol")
-   (check-equal? (read_string "true") #t "Symbol")
-   (check-equal? (read_string "false") #f "Symbol")
-   (check-equal? (read_string "nil") nil "Symbol")
+  ; symbol
+  (check-equal? (read_string "pqr") 'pqr "Symbol")
+  (check-equal? (read_string "true") #t "Symbol")
+  (check-equal? (read_string "false") #f "Symbol")
+  (check-equal? (read_string "nil") nil "Symbol")
 
-   ; Correctly handle comment then atom
-   (define r (new token-reader% [target-string ";comment\n21"]))
-   (check-equal? (read-form r) white-space "Comment then number - comment")
-   (check-equal? (read-form r) 21 "Comment then number - number")
-   )
+  ; Correctly handle comment then atom
+  (define r (new token-reader% [target-string ";comment\n21"]))
+  (check-equal? (read-form r) white-space "Comment then number - comment")
+  (check-equal? (read-form r) 21 "Comment then number - number")
+  )
